c57bd7d7a1456ff81c05f60d679b7aa9166215a6
AndyEssomba <146026431+AndyEssomba@users.noreply.github.com>
2024-06-06 20:53:57 +0100

    modified:   Main/Dashboard.html 	modified:   Main/Js.js 	deleted:    Main/Dasboard.css 	modified:   Main/Dashboard.html 	modified:   Main/Js.js

--- Main/Js.js
+++ Main/Js.js
@@ -14,10 +14,13 @@ function toggleMode() {
     innerbutton.forEach(button => {
       button.style.color = 'white'; // Change text color to white
     });
+    $('#modes').attr('src', "/Related-files/icons8-soleil-50.png");
   } else {
     leftMenu.classList.remove('dark-mode');
     innerbutton.forEach(button => {
     button.style.color = 'black'; // Change text color to black
     });
+    $('#modes').attr('src', '/Related-files/icons8-croissant-de-lune-50.png');
+
   }
 }
